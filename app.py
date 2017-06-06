c85c1446518ace5db911aab6ecbee1172a613e9b
GreywindGit <GreywindGit@users.noreply.github.com>
2017-06-06 17:04:55 +0200

    Content for root and list routes

--- app.py
+++ app.py
@@ -6,7 +6,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    table = bll.get_five_recent_questions()
+    return render_template('index.html', table=table, page_title='Main Page')
+
+
+@app.route('/list')
+def list_all_questions():
+    table = bll.get_all_questions()
+    return render_template('list.html', table=table, page_title='Questions')
 
 
 if __name__ == '__main__':
--- bll.py
+++ bll.py
@@ -1,1 +1,12 @@
 import dal
+
+
+def get_five_recent_questions():
+    sql_string = 'SELECT submission_time, view_number, vote_number, title FROM question ORDER BY submission_time DESC LIMIT 5;'
+    return dal.get_data_from_table(sql_string)
+
+
+def get_all_questions():
+    sql_string = 'SELECT submission_time, view_number, vote_number, title FROM question ORDER BY submission_time;'
+    return dal.get_data_from_table(sql_string)
+
